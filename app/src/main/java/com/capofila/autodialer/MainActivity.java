d369e7ddd924786a10e3e8709922b30c5f6f9bc1
sk1794 <kumar1234shubham@gmail.com>
2019-09-25 18:31:33 +0530

    added new activity for call details

--- app/src/main/java/com/capofila/autodialer/MainActivity.java
+++ app/src/main/java/com/capofila/autodialer/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.location.SettingInjectorService;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
